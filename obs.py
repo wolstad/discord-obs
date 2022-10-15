cf4b39cf833b0564f5ce7de32e954e75dbc9d5d2
Wes <wcolstad@protonmail.com>
2022-10-15 10:03:26 -0700

    missing paren

--- obs.py
+++ obs.py
@@ -1,13 +1,14 @@
+from ipaddress import ip_address
 import simpleobsws
 import config
 
 # Basic config
 parameters = simpleobsws.IdentificationParameters(ignoreNonFatalRequestChecks = False)
-ws = simpleobsws.WebSocketClient(url = f"ws://{config.get_ip}:{config.get_port}", password = config.get_password, identification_parameters = parameters)
+ws = simpleobsws.WebSocketClient(url = f"ws://{config.get_ip()}:{config.get_port()}", password = config.get_password(), identification_parameters = parameters)
 
 # Connect to websocket
 async def connect():
-    print(f"Attempting to connect to OBS Websocket: ws://{config.get_ip}:{config.get_port}")
+    print(f"Attempting to connect to OBS Websocket: ws://{config.get_ip()}:{config.get_port()}")
     await ws.connect() 
     await ws.wait_until_identified()
 
